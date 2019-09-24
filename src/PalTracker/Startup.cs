53341610fd8eb4acda4e7bdd87d4a9eae1837615
Dhananjay Kushwaha <d.kushwaha@gmail.com>
2019-09-24 16:59:20 +0530

    Integrated DB with CICD

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -34,7 +35,9 @@ namespace PalTracker
             services.AddSingleton(sp => new WelcomeMessage(Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")));
             services.AddSingleton(sp => new CloudFoundryInfo(port, memoryLimit, cfInstanceIndex, cfInstanceAddr));
 
-            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            //services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- test/PalTrackerTests/DbTestSupport.cs
+++ test/PalTrackerTests/DbTestSupport.cs
@@ -1,46 +1,46 @@
-﻿// using System.Collections.Generic;
-// using System.Linq;
-// using MySql.Data.MySqlClient;
+﻿using System.Collections.Generic;
+using System.Linq;
+using MySql.Data.MySqlClient;
 
-// namespace PalTrackerTests
-// {
-//     public static class DbTestSupport
-//     {
-//         public const string TestDbConnectionString = "server=127.0.0.1;uid=tracker_dotnet;pwd=password;database=tracker_dotnet_test";
+namespace PalTrackerTests
+{
+    public static class DbTestSupport
+    {
+        public const string TestDbConnectionString = "server=127.0.0.1;uid=tracker_dotnet;pwd=password;database=tracker_dotnet_test";
 
-//         public static IList<IDictionary<string, object>> ExecuteSql(string sql)
-//         {
-//             var result = new List<IDictionary<string, object>>();
+        public static IList<IDictionary<string, object>> ExecuteSql(string sql)
+        {
+            var result = new List<IDictionary<string, object>>();
 
-//             using (var connection = new MySqlConnection(TestDbConnectionString))
-//             {
-//                 connection.Open();
+            using (var connection = new MySqlConnection(TestDbConnectionString))
+            {
+                connection.Open();
 
-//                 using (var command = connection.CreateCommand())
-//                 {
-//                     command.CommandText = sql;
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = sql;
 
-//                     using (var reader = command.ExecuteReader())
-//                     {
-//                         while (reader.HasRows)
-//                         {
-//                             while (reader.Read())
-//                             {
-//                                 var rowData = Enumerable.Range(0, reader.FieldCount)
-//                                     .ToDictionary(reader.GetName, reader.GetValue);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                var rowData = Enumerable.Range(0, reader.FieldCount)
+                                    .ToDictionary(reader.GetName, reader.GetValue);
 
-//                                 result.Add(rowData);
-//                             }
+                                result.Add(rowData);
+                            }
 
-//                             reader.NextResult();
-//                         }
-//                     }
-//                 }
+                            reader.NextResult();
+                        }
+                    }
+                }
 
-//                 connection.Close();
-//             }
+                connection.Close();
+            }
 
-//             return result;
-//         }
-//     }
-// }
+            return result;
+        }
+    }
+}
